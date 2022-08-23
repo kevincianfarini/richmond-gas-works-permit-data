fdf77c0790aacd58a899795d1a2772a53dced604
Kevin Cianfarini <kevincianfarini@gmail.com>
2022-08-23 18:18:18 -0400

    Print better output

--- src/commonMain/kotlin/org/climatechangemakers/permitdata/main.kt
+++ src/commonMain/kotlin/org/climatechangemakers/permitdata/main.kt
@@ -3,9 +3,11 @@ package org.climatechangemakers.permitdata
 import app.cash.sqldelight.EnumColumnAdapter
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import io.ktor.utils.io.core.*
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.collectIndexed
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
@@ -35,28 +37,28 @@ private suspend fun collectData() {
       coroutineScope {
         PermitType.values().forEach { type ->
           launch {
-            send(service.getPermitData(type))
+            service.getPermitData(type).result.permitResults.forEach { permit ->
+              send(permit)
+            }
           }
         }
       }
     }
-  }.buffer(PermitType.values().size).collect { data ->
-    println("Inserting ${data.result.permitResults.size} entires...")
-    data.result.permitResults.forEach { record ->
-      database.permitQueries.insert(
-        status = record.status,
-        issuedDate = record.issuedDate,
-        applicationDate = record.applyDate,
-        expirationDate = record.expireDate,
-        completionDate = record.completeDate,
-        finalDate = record.finalDate,
-        requestDate = record.requestDate,
-        city = record.address?.city,
-        postalCode = record.address?.postalCode,
-        fullAddress = record.address?.fullAddress,
-        reason = record.permitType.reason,
-        destination = record.permitType.installationDestination,
-      )
-    }
+  }.buffer(Channel.UNLIMITED).collectIndexed { index, record ->
+    database.permitQueries.insert(
+      status = record.status,
+      issuedDate = record.issuedDate,
+      applicationDate = record.applyDate,
+      expirationDate = record.expireDate,
+      completionDate = record.completeDate,
+      finalDate = record.finalDate,
+      requestDate = record.requestDate,
+      city = record.address?.city,
+      postalCode = record.address?.postalCode,
+      fullAddress = record.address?.fullAddress,
+      reason = record.permitType.reason,
+      destination = record.permitType.installationDestination,
+    )
+    println("Inserted ${index + 1} records...")
   }
 }
